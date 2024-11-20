b0447e4ca714553a5b761039c94dbd5bbbbbb5e6
Harish <21jr1a1262@gmail.com>
2024-11-20 21:47:58 +0530

    redux added

--- src/reducer/index.jsx
+++ src/reducer/index.jsx
@@ -1,1 +1,9 @@
-import {combineReducer} from ;
+// import {combineReducer} from "@reduxjs/toolkit" ;
+import combineReducer from "@reduxjs/toolkit";
+import authReducer from "../slices/authSlice";
+const rootReducer = combineReducer({
+
+    auth: authReducer,
+})
+
+export default rootReducer;
