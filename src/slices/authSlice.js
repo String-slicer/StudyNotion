e68a19feac0a0e0fc5280f2d0d5d6a70f921662d
Harish <21jr1a1262@gmail.com>
2024-11-22 21:57:28 +0530

    added comments

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,5 +1,5 @@
 import {createSlice} from "redux-toolkit"
-
+//  here slices are created to store data and access the data across the whole website 
 const initialState ={
     token : localStorage.getItem("token")?JSON.parse(localStorage.getItem("token")):null,
 };
